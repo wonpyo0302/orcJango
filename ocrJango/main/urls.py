be4a20b7599d15a265bc680c5a6f5ddccdb1f6d0
wonpyo0302 <pyome2323@gmail.com>
2024-06-05 17:17:16 +0900

    gptApi-commit

--- ocrJango/main/urls.py
+++ ocrJango/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.helloworld, name='helloworld'),
+    path('checkdata', views.checkdata, name='checkdata'),
 ]
--- ocrJango/main/views.py
+++ ocrJango/main/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
+import json
+from openai import OpenAI
+import os
 
 
 # Create your views here.
 def helloworld(request):
-    return render(request, 'ocrJango/helloworld.html')
+    return render(request, 'ocrJango/helloworld.html', {})
 
+
+@require_POST
+def checkdata(request):
+    client = OpenAI(
+        api_key = os.environ["OPENAI_API_KEY"]
+    )
+    jsonObj = json.loads(request.body)
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant in the summary"},
+        {"role": "user", "content": f"Summarize the following. \n {jsonObj.get('contents')}"}
+    ]
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=4000,
+        temperature=0.8,
+        n=1
+    )
+
+    resData = {
+        'result':response.choices[0].message.content
+    }
+
+    return JsonResponse(resData)
