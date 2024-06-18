4f847e670affcd861d1c0b8304fb3ad27977f098
anjp0304 <anjp0304@gmail.com>
2024-06-19 00:46:57 +0900

    processOcrImg - (2)

--- ocrJango/main/urls.py
+++ ocrJango/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.helloworld, name='helloworld'),
     path('processCutImg', views.processCutImg, name='processCutImg'),
+    path('processOcrImg', views.processOcrImg, name='processOcrImg'),
 ]
